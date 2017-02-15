a6d262048e537b3c3a76f42e7fcc3fc9d92a66e0
Travis Blasingame <stygian.warlock.v2@gmail.com>
2017-02-15 13:01:25 -0600

    Update Seeder.php

--- src/JumpGate/Core/Abstracts/Seeder.php
+++ src/JumpGate/Core/Abstracts/Seeder.php
@@ -6,12 +6,33 @@ use Illuminate\Support\Facades\DB;
 
 abstract class Seeder extends \Illuminate\Database\Seeder
 {
+    /**
+     * @var \Illuminate\Database\DatabaseManager
+     */
+    protected $db;
+    
+    /**
+     * @param \Illuminate\Database\DatabaseManager $db
+     * @param \Illuminate\Filesystem\Filesystem    $files
+     */
+    public function __construct(DatabaseManager $db)
+    {
+        parent::__construct();
+
+        $this->db    = $db;
+    }
+    
+    /**
+     * Truncate the existing table of all records.
+     *
+     * @param string $table
+     */
     protected function truncate($table)
     {
-        if (DB::connection()->getConfig('driver') === 'mysql') {
-            DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-            DB::table($table)->truncate();
-            DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        if ($this-db->connection()->getConfig('driver') === 'mysql') {
+            $this-db->statement('SET FOREIGN_KEY_CHECKS=0;');
+            $this-db->table($table)->truncate();
+            $this-db->statement('SET FOREIGN_KEY_CHECKS=1;');
         }
     }
 }
