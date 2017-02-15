a1fe58f392373d50318b82b30007c525741dafbc
Travis Blasingame <stygian.warlock.v2@gmail.com>
2017-02-15 12:11:52 -0600

    Create Seeder.php

---
+++ src/JumpGate/Core/Abstracts/Seeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace JumpGate\Core\Abstracts;
+
+abstract class BaseSeeder extends \Illuminate\Database\Seeder
+{
+    protected function truncate($table)
+    {
+        if (DB::connection()->getConfig('driver') === 'mysql') {
+            DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+            DB::table($table)->truncate();
+            DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        }
+    }
+}
